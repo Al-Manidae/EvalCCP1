b460a7fd734a41c0e1884e24f23579dee6fac5e3
Alexandre Gauthier <79856285+Al-Manidae@users.noreply.github.com>
2023-10-25 23:16:28 +0200

    Update signUp.js

--- javascript/signUp.js
+++ javascript/signUp.js
@@ -24,7 +24,7 @@ const isEmailValid = (email) => {
 
 // Vérif du mdp ------------------------------
 const isPasswordSecure = (password) => {
-    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#+-^\[\]])(?=.{8,})");
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#+\-^\[\]])(?=.{8,})");
     return re.test(password);
 };
 
