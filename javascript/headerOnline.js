e94bcba735c42c97c52a80f038701d0e89c1a200
Alexandre Gauthier <alexandre.gauthier.soul@hotmail.fr>
2023-10-26 14:51:53 +0200

    header online & burger fonctionnels (responsivé

---
+++ javascript/headerOnline.js
@@ -0,0 +1,42 @@
+// Récupérer le nom
+let nom = localStorage.getItem('nom');
+
+//Récupération de la date
+let dateLogIn = new Date(localStorage.getItem('dateLogIn'));
+let currentDate = new Date(),
+    currentDay = currentDate.getDate(),
+    currentMonth = currentDate.getMonth()+1,
+    currentYear = currentDate.getFullYear();
+console.log(currentDate)
+
+document.getElementById("nom").innerHTML = nom;
+document.getElementById("modalNom").innerHTML = nom;
+
+if (currentDay<10) {currentDay="0"+currentDay}
+if (currentMonth<10) {currentMonth="0"+currentMonth}
+document.getElementById("date").innerHTML = currentDay+"/"+currentMonth+"/"+currentYear;
+document.getElementById("modalDate").innerHTML = currentDay+"/"+currentMonth+"/"+currentYear;
+
+document.getElementById("heure").innerHTML = dateLogIn.getHours()+":"+dateLogIn.getMinutes()+":"+dateLogIn.getSeconds();
+document.getElementById("modalHeure").innerHTML = dateLogIn.getHours()+":"+dateLogIn.getMinutes()+":"+dateLogIn.getSeconds();
+
+let burgerOpn = document.getElementById("burgerOpn"),
+    overlay = document.getElementById("overlay"),
+    burgerModal = document.getElementById("burgerModal")
+    burgerClo = document.getElementById("burgerClo")
+;
+
+burgerOpn.addEventListener("click", function burgerMenu() {
+    overlay.classList.toggle("show");
+    burgerModal.classList.toggle("show");
+}, false);
+
+burgerClo.addEventListener("click", function burgerMenu() {
+    overlay.classList.toggle("show");
+    burgerModal.classList.toggle("show");
+}, false);
+
+overlay.addEventListener("click", function burgerMenu() {
+    overlay.classList.toggle("show");
+    burgerModal.classList.toggle("show");
+}, false);
